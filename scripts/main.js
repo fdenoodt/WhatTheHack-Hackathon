725494fdfc9e39828dd948b5be5f211853fd685a
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-03-01 21:06:25 +0100

    navigation + materialdesign

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const init = () => {
+  const pages = ['home', 'login', 'register']
+  for (const page of pages) {
+    document.querySelector('.' + page).style.display = 'none'
+  }
+
+  router = new Router(pages)
+}
